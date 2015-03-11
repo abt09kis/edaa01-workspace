aea8d6b391429aeaa9c1c66b02562832ac3a2da5
Kevin Ispiroglu Lindblad <abt09kis@student.lu.se>
2015-03-11 14:30:46 +0100

    Får inte rätt på sista delen på labb 4

--- lab4/src/bst/testTree.java
+++ lab4/src/bst/testTree.java
@@ -8,21 +8,31 @@ public class testTree{
 	
 	
 	public static void main(String[]args){
+		Random rand = new Random();
 		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
 		BSTVisualizer visualizer = new BSTVisualizer("Tree", 600, 600);
-		tree.add(5);
-		tree.add(5);
+		/**
+		tree.add(9);
 		tree.add(3);
 		tree.add(2);
 		tree.add(7);
+		*/
+		for (int i = 0; i < 15; i++){
+			tree.add(rand.nextInt(69));
+		}
 		
-		int höjd = 	tree.height();
+	
+		
 		int storlek = tree.size();
+		int höjd = 	tree.height();
 		
 		System.out.println(höjd);
 		System.out.println(storlek);
 		
-		tree. printTree();
+		tree.printTree();
+				
+		
+		tree.rebuild();
 		
 		visualizer.drawTree(tree);
 		
